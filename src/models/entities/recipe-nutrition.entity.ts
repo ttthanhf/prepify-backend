52b88b2d2c5c27ade622d5cc2ce97e07a98f8ea0
ttthanhf <thanhvip300@gmail.com>
2024-07-02 00:14:03 +0700

    [226] Fix create recipe

--- src/models/entities/recipe-nutrition.entity.ts
+++ src/models/entities/recipe-nutrition.entity.ts
@@ -1,29 +1,18 @@
-import {
-	Column,
-	Entity,
-	JoinColumn,
-	ManyToOne,
-	PrimaryColumn,
-	Relation
-} from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryColumn, Relation } from 'typeorm';
 import { Nutrition } from './nutrition.entity';
 import { Recipe } from './recipe.entity';
 import { Unit } from './unit.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Entity({ name: 'recipe_nutrition' })
 export class RecipeNutrition {
-	@PrimaryColumn()
-	recipe_id!: string;
-
-	@PrimaryColumn()
-	nutrition_id!: string;
+	@PrimaryColumn({ type: 'uuid' })
+	id: string = uuidv4();
 
 	@ManyToOne(() => Recipe, (recipe) => recipe.recipeNutritions)
-	@JoinColumn({ name: 'recipe_id' })
 	recipe!: Relation<Recipe>;
 
 	@ManyToOne(() => Nutrition, (nutrition) => nutrition.recipeNutritions)
-	@JoinColumn({ name: 'nutrition_id' })
 	nutrition!: Nutrition;
 
 	@Column()
